2427bb07949ecc4b51c9ce4f8b21b76c1968b1ce
Unknown <joakimbensryd@gmail.com>
2018-09-17 18:32:25 +0200

    Main Game Loop

--- Eastwood/Eastwood/main.cpp
+++ Eastwood/Eastwood/main.cpp
@@ -1,4 +1,30 @@
+#include "sfml_libs.h"
+
+#include "SFML/Graphics.hpp"
 
 int main()
 {
+	sf::VideoMode vm;
+	vm.width = 1600;
+	vm.height = 900;
+	vm.bitsPerPixel = 32;
+
+	sf::RenderWindow window(vm, "Eastwood");
+
+	sf::Event e;
+	while (window.isOpen())
+	{
+		while (window.pollEvent(e))
+		{
+			if (e.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear(sf::Color::Magenta);
+		window.display();
+	}
+
+	return 0;
 }

3166eb227b9ae155c7331561a204412010f2185b
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2018-09-19 21:45:31 +0100

    Changed Renderable Component to use TransformComponent

--- Eastwood/Eastwood/RenderableComponent.cpp
+++ Eastwood/Eastwood/RenderableComponent.cpp
@@ -1,6 +1,7 @@
 #include "RenderableComponent.h"
 #include "GameObject.h"
 #include "Renderer.h"
+#include "TransformComponent.h"
 
 CRenderableComponent::CRenderableComponent()
 	: CComponent(this, sizeof(CRenderableComponent))
@@ -19,11 +20,18 @@ void CRenderableComponent::Tick(float aDT)
 {
 	CGameObject& owner = GetOwner();
 
-	mySprite.setPosition(owner.GetPosition());
+	CTransformComponent* transformComponent = GetComponentOfType(GetOwner(), CTransformComponent);
+
+	mySprite.setPosition(transformComponent->GetPosition());
 
 	myRendererToUse->PushRenderCommand(mySprite);
 }
 
 void CRenderableComponent::OnDestroy()
 {
+}
+
+ComponentType CRenderableComponent::GetType() const
+{
+	return typeid(CRenderableComponent);
 }

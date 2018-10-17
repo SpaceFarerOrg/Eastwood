af6147c5d85e808ef93003b2773e69cff6878b0d
Unknown <joakimbensryd@gmail.com>
2018-10-17 23:12:56 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Eastwood/Eastwood/ControlCompent.cpp
@@ -0,0 +1,28 @@
+#include "ControlCompent.h"
+#include "InputManager.h"
+#include "CommonMath.h"
+
+sf::Vector2f CControlComponent::GetDirection()
+{
+	CInputManager& im = CInputManager::GetInstance();
+	sf::Vector2f dir;
+
+	if (im.IsKeyDown(EKeyCode::Left) || im.IsKeyDown(EKeyCode::A))
+	{
+		dir.x += -1;
+	}
+	if (im.IsKeyDown(EKeyCode::Up) || im.IsKeyDown(EKeyCode::W))
+	{
+		dir.y -= 1;
+	}
+	if (im.IsKeyDown(EKeyCode::Right) || im.IsKeyDown(EKeyCode::D))
+	{
+		dir.x += 1;
+	}
+	if (im.IsKeyDown(EKeyCode::Down) || im.IsKeyDown(EKeyCode::S))
+	{
+		dir.y += 1;
+	}
+
+	return Math::GetNormalized(dir);
+}
